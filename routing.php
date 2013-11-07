31042fb9e91503c75bb6cf623e6d4668cbc6352c
Maxime Plancke <maxime.plancke@gmail.com>
2013-11-07 14:40:23 +0100

    Modif slash routing

--- routing.php
+++ routing.php
@@ -6,43 +6,43 @@ $template = null;
 
 $config_routing = array(
   	'log_in' => array(
-    	'module' => '/account',
+    	'module' => 'account',
     	'action' => 'log_in'
   	),
   	'registration' => array(
-    	'module' => '/account',
+    	'module' => 'account',
     	'action' => 'registration'
   	),
   	'log_out' => array(
-    	'module' => '/account',
+    	'module' => 'account',
     	'action' => 'log_out'
   	),
   	'delete_account' => array(
-    	'module' => '/account',
+    	'module' => 'account',
     	'action' => 'delete_account'
   	),
   	'current_obj' => array(
-    	'module' => '/user',
+    	'module' => 'user',
     	'action' => 'current_obj'
   	),
   	'current_obj_advices' => array(
-    	'module' => '/user',
+    	'module' => 'user',
     	'action' => 'current_obj_advices'
   	),
     'add_advice' => array(
-    	'module' => '/user',
+    	'module' => 'user',
     	'action' => 'add_advice'
   	),
   	'add_friends' => array(
-    	'module' => '/user',
+    	'module' => 'user',
     	'action' => 'add_friends'
   	),
   	'new_objective' => array(
-    	'module' => '/objective',
+    	'module' => 'objective',
     	'action' => 'new_objective'
   	),
   	'home' => array(
-    	'module' => '/home',
+    	'module' => 'home',
     	'action' => 'home'
   	)
 );
@@ -51,7 +51,7 @@ $param_routing = $config_routing[$page];
 $module = $param_routing['module'];
 $action = $param_routing['action'];
 
-include ($_SERVER['DOCUMENT_ROOT'].$module."/controlleur/".$action.".php");
-$template = $_SERVER['DOCUMENT_ROOT'].$module."/template/".$action.".php";
+include ($_SERVER['DOCUMENT_ROOT']."/".$module."/controlleur/".$action.".php");
+$template = $_SERVER['DOCUMENT_ROOT']."/".$module."/template/".$action.".php";
 
 ?>
