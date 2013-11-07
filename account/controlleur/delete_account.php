bdd9105d8d991a34725d59618c8abf9da0362931
Maxime Plancke <maxime.plancke@gmail.com>
2013-11-07 15:43:30 +0100

    Probleme indentation

--- account/controlleur/delete_account.php
+++ account/controlleur/delete_account.php
@@ -2,14 +2,14 @@
 $user_id = isset($_SESSION['id']) ? $_SESSION['id'] : null;
 $errors = array();
 
-    if ($user_id) {
-    	$delete_user = $bdd->query ('DELETE FROM membres WHERE id ='.$user_id);
-        $delete_user->closeCursor();
-    	$_SESSION = array();
-		session_destroy();
-    	header('Location:registration.html');
-    	exit();
-    }
-    header('Location:registration.html');
-    exit();
+if ($user_id) {
+	$delete_user = $bdd->query ('DELETE FROM membres WHERE id ='.$user_id);
+    $delete_user->closeCursor();
+	$_SESSION = array();
+	session_destroy();
+	header('Location:registration.html');
+	exit();
+}
+header('Location:registration.html');
+exit();
 ?>
