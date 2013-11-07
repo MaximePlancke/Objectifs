e4abdb482d768fe30cc0a4d6a401e7c3fa2a1e4c
Maxime Plancke <maxime.plancke@gmail.com>
2013-11-07 15:21:41 +0100

    Ajout des variables URL

--- routing.php
+++ routing.php
@@ -8,42 +8,52 @@ $config_routing = array(
   	'log_in' => array(
     	'module' => 'account',
     	'action' => 'log_in'
+    	'url' => 'log_in.html'
   	),
   	'registration' => array(
     	'module' => 'account',
     	'action' => 'registration'
+    	'url' => 'registration.html'
   	),
   	'log_out' => array(
     	'module' => 'account',
     	'action' => 'log_out'
+    	'url' => 'log_out.html'
   	),
   	'delete_account' => array(
     	'module' => 'account',
     	'action' => 'delete_account'
+    	'url' => 'delete_account.html'
   	),
   	'current_obj' => array(
     	'module' => 'user',
     	'action' => 'current_obj'
+    	'url' => 'current_obj.html'
   	),
   	'current_obj_advices' => array(
     	'module' => 'user',
     	'action' => 'current_obj_advices'
+    	'url' => 'current_obj_advices.html'
   	),
     'add_advice' => array(
     	'module' => 'user',
     	'action' => 'add_advice'
+    	'url' => 'add_advice.html'
   	),
   	'add_friends' => array(
     	'module' => 'user',
     	'action' => 'add_friends'
+    	'url' => 'add_friends.html'
   	),
   	'new_objective' => array(
     	'module' => 'objective',
     	'action' => 'new_objective'
+    	'url' => 'new_objective.html'
   	),
   	'home' => array(
     	'module' => 'home',
     	'action' => 'home'
+    	'url' => 'index.html'
   	)
 );
 
